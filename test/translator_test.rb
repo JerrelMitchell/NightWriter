d0d225093cfac61fa72288ddd8ae81de44f4f0a9
JerrelMitchell <jerrelmitchell@ymail.com>
2018-03-24 22:55:39 -0600

    add tests for translator class. update translator class file to include alphabet hash for letter and braille key-value pairs.

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -3,4 +3,72 @@ require 'minitest/pride'
 require './lib/translator'
 
 class TranslatorTest < Minitest::Test
+  def test_can_translate_one_lowercase_character
+    translator = Translator.new
+    expected   = ["0.", "..", ".0"]
+    actual     = translator.translate_to_braille("k")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_multiple_lowercase_characters
+    translator = Translator.new
+    expected   = ["0.", "..", ".0","0.","..",".."]
+    actual     = translator.translate_to_braille("ka")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_one_uppercase_character
+    translator = Translator.new
+    expected   = ["..","..",".0","0.","..",".."]
+    actual     = translator.translate_to_braille("A")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_multiple_uppercase_characters
+    translator = Translator.new
+    expected   = ["..","..",".0","0.","..","..","..","..",".0","0.", ".0", "00"]
+    actual     = translator.translate_to_braille("AZ")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_multiple_upper_and_lowercase_characters
+    translator = Translator.new
+    expected   = ["..", "..", ".0", "0.", "..", "..", "0.", "0.", "..", "..", "..", ".0", "0.", "..", "..", "00", "..", ".."]
+    actual     = translator.translate_to_braille("AbAc")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_spaces
+    translator = Translator.new
+    expected   = ["0.", ".0", "00","..","..","..","..","..",".0","0.","..",".."]
+    actual     = translator.translate_to_braille("z A")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_a_punctuation_mark
+    translator = Translator.new
+    expected   = ["..", "0.", "00"]
+    actual     = translator.translate_to_braille("?")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_multiple_punctuation_marks
+    translator = Translator.new
+    expected   = ["..", "00", ".0", "..", "..", "00"]
+    actual     = translator.translate_to_braille(".-")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_a_full_sentence
+    translator = Translator.new
+    # YOUR CODE HERE
+  end
 end

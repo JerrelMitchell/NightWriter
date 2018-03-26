8cde173b4cb04d372132c8e52ac696f5e48fb1a1
JerrelMitchell <jerrelmitchell@ymail.com>
2018-03-25 18:48:52 -0600

    update alphabet for letter and number shift keys and values, add tests to include number and number shift.

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -14,7 +14,6 @@ class TranslatorTest < Minitest::Test
   end
 
   def test_can_translate_a_different_lowercase_character
-
     translator = Translator.new
     expected   = ["00", "..", ".."]
     actual     = translator.english_to_braille("c")
@@ -23,7 +22,6 @@ class TranslatorTest < Minitest::Test
   end
 
   def test_can_translate_multiple_lowercase_characters
-
     translator = Translator.new
     expected   = ["0.", "..", "0.", "0.", "..", ".."]
     actual     = translator.english_to_braille("ka")
@@ -32,43 +30,38 @@ class TranslatorTest < Minitest::Test
   end
 
   def test_can_translate_one_uppercase_character
-
     translator = Translator.new
-    expected   = ["..0.", "....", ".0.."]
+    expected   = ["..", "..", ".0", "0.", "..", ".."]
     actual     = translator.english_to_braille("A")
 
     assert_equal expected, actual
   end
 
   def test_can_translate_multiple_uppercase_characters
-
     translator = Translator.new
-    expected   = ["..0.", "..0.", ".0..", "..0.", "...0", ".000"]
+    expected   = ["..", "..", ".0", "0.", "0.", "..", "..", "..", ".0", "0.", ".0", "00"]
     actual     = translator.english_to_braille("BZ")
 
     assert_equal expected, actual
   end
 
   def test_can_translate_multiple_upper_and_lowercase_characters
-
     translator = Translator.new
-    expected   = ["..0.", "....", ".0..", "0.", "0.", "..", "..00", "....", ".0..", "00", ".0", ".."]
+    expected   = ["..", "..", ".0", "0.", "..", "..", "0.", "0.", "..", "..", "..", ".0", "00", "..", "..", "00", ".0", ".."]
     actual     = translator.english_to_braille("AbCd")
 
     assert_equal expected, actual
   end
 
   def test_can_translate_spaces
-
     translator = Translator.new
-    expected   = ["0.", ".0", "00","..", "..", "..", "..0.", "...0", ".00."]
+    expected   = ["0.", ".0", "00", "..", "..", "..", "..", "..", ".0", "0.", ".0", "0."]
     actual     = translator.english_to_braille("z O")
 
     assert_equal expected, actual
   end
 
   def test_can_translate_a_punctuation_mark
-
     translator = Translator.new
     expected   = ["..", "0.", "00"]
     actual     = translator.english_to_braille("?")
@@ -77,7 +70,6 @@ class TranslatorTest < Minitest::Test
   end
 
   def test_can_translate_multiple_punctuation_marks
-
     translator = Translator.new
     expected   = ["..", "00", ".0", "..", "..", "00"]
     actual     = translator.english_to_braille(".-")
@@ -85,11 +77,27 @@ class TranslatorTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  def test_can_translate_a_full_sentence
-
+  def test_can_translate_number
     translator = Translator.new
-    expected   = ["..00", "....", ".00.", ".0", "0.", "..", "0.", "..", "0.", "0.", ".0", "..", "..","..","0.", ".0", "0.", "0.", "..", "..", "..", "0.", ".0", "00", "0.", "..", ".."]
-    actual     = translator.english_to_braille("Mike's za")
-    assert_equal expected, actual 
+    expected   = [".0", ".0", "00", "0.", ".0", ".."]
+    actual     = translator.english_to_braille("5")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_multiple_numbers
+    translator = Translator.new
+    expected   = [".0", ".0", "00", "0.", "0.", "..", ".0", ".0", "00", ".0", "0.", ".."]
+    actual     = translator.english_to_braille("29")
+
+    assert_equal expected, actual
+  end
+
+  def test_can_translate_a_full_sentence
+    translator = Translator.new
+    expected   = ["..", "..", ".0", "00", "..", "0.", ".0", "0.", "..", "0.", "..", "0.", "0.", ".0", "..", "..", "..", "0.", ".0", "0.", "0.", "..", "..", "..", ".0", ".0", "00", "00", "0.", "..", ".0", ".0", "00", "0.", "0.", "..", "00", ".0", "0.", "00", ".0", "..", "..", "..", "..", "..", "..", "0.", "0.", ".0", "00", "0.", ".0", "00", "0.", "..", "..", "..", "0.", "00", "..", "00", "0."]
+    actual     = translator.english_to_braille("Mike's 62nd 'zza?!")
+
+    assert_equal expected, actual
   end
 end

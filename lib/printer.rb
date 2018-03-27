a3f6fcd27ac55cfde63fd601ad3c186e774430e5
JerrelMitchell <jerrelmitchell@ymail.com>
2018-03-26 21:14:32 -0600

    add method and testing for slice_to_tracks to work on writer file implementation from printer class

--- lib/printer.rb
+++ lib/printer.rb
@@ -14,30 +14,4 @@ class Printer
       track3 << character[2]
     end
   end
-
-  def fill_tracks(character)
-    track1 << character[0]
-    track2 << character[1]
-    track3 << character[2]
-  end
-
-  def end_track(track)
-    track.slice!(0..79) << "\n"
-  end
-
-  def tracks_into_output(track)
-    until track.empty?
-      output_text << end_track(track1)
-      output_text << end_track(track2)
-      output_text << end_track(track3)
-    end
-  end
-
-  def print_tracks(characters)
-    characters.each_slice(3) do |character|
-      fill_tracks(character)
-    end
-    tracks_into_output(track1)
-    output_text
-  end
 end
